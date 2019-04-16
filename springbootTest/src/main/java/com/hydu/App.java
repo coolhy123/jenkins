98e2bad15a72b7f8e146cee21f52dbfe5d6f9501
heyong <coolhy123@163.com>
2019-04-16 10:40:30 +0800

    测试jenkins

---
+++ springbootTest/src/main/java/com/hydu/App.java
@@ -0,0 +1,16 @@
+package com.hydu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("springboot+jenkins∆Ù∂Ø≤‚ ‘");
+		SpringApplication.run(App.class, args);
+
+	}
+
+}
